9673f54d2f5fea2cd185c00b161df4538cdb0181
OLAJIRE <olajirefolasore@gmail.com>
2026-02-06 07:24:56 +0100

    Added login and logout functions

--- swiftlink/urls.py
+++ swiftlink/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path 
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'swiftlink'
 
 urlpatterns = [
     path('', views.shortener, name='home'),
-    path('<str:short_key>/', views.redirect_url, name='redirect')
+    path('register/', views.register, name='register'),
+    #path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='swiftlink/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('<str:short_key>/', views.redirect_url, name='redirect'),
 ]
